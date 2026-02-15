b62df105a317800187be5bbf446dc2d4a8cf7082
2025ab05107 <2025ab05107@wilp.bits-pilani.ac.in>
2026-02-15 08:06:02 +0530

    Update M2-train_model.py

--- model/M2-train_model.py
+++ model/M2-train_model.py
@@ -16,8 +16,8 @@ from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, reca
 # Load dataset
 df = pd.read_csv("../data/heart_disease_uci.csv")
 
-X = df.drop("target", axis=1)
-y = df["target"]
+X = df.drop("num", axis=1)
+y = df["num"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
